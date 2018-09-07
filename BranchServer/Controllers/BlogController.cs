02e9d37db255c517dcf6369a88b51b0657b2699b
Decklan <decklan007@gmail.com>
2018-09-07 13:29:22 -0700

    Added annotations and comments to BlogController methods for consistency, modified GetAllBlogsAsync to map to resources, added .vscode/ to .gitignore since I don't want to track that crap.

--- BranchServer/Controllers/BlogController.cs
+++ BranchServer/Controllers/BlogController.cs
@@ -12,11 +12,23 @@ namespace BranchServer
     [Route("api/[controller]")]
     public class BlogController : Controller
     {
+        /**
+         * _context contains the various DbSets that we operate
+         * on including:
+         * * Blogs
+         * * Auth
+         */
         private readonly BranchContext _context;
         public BlogController(BranchContext context) {
             _context = context;
         }
 
+        /// <summary>
+        /// Adds a single blog post to the database and returns the newly
+        /// created post back to the client
+        /// </summary>
+        /// <param name="blogResource">The blog that is being sent to the server for creation</param>
+        /// <returns>The newly created blog that was stored in the database</returns>
         [HttpPost]
         public async Task<IActionResult> AddBlogAsync([FromBody] BlogResource blogResource) {
             if (!ModelState.IsValid) {
@@ -36,6 +48,11 @@ namespace BranchServer
 
             blog = await _context.Blogs.FirstOrDefaultAsync(newBlog => newBlog.BlogId == blog.BlogId);
 
+            if (blog == null)
+            {
+                return NotFound("Couldn't find the newly created blog.");
+            }
+
             var resource = new BlogResource {
                 BlogId = blog.BlogId,
                 Title = blog.Title,
@@ -47,12 +64,42 @@ namespace BranchServer
             return Ok(resource);
         }
 
+        /// <summary>
+        /// Asynchronously fetches the entire collection of blogs to
+        /// return to the client
+        /// </summary>
+        /// <returns>A list of all current blogs</returns>
         [HttpGet]
         public async Task<IActionResult> GetAllBlogsAsync() {
             var blogs = await _context.Blogs.ToListAsync();
-            return Ok(blogs);
+
+            if (blogs == null)
+            {
+                return NotFound("There are currently no blog posts in the db.");
+            }
+
+            // Create list of resources from list of backend models
+            var blogResources = new List<BlogResource>();
+            blogs.ForEach(blog => {
+                var resource = new BlogResource
+                {
+                    BlogId = blog.BlogId,
+                    Title = blog.Title,
+                    Body = blog.Body,
+                    CreationDate = blog.CreationDate,
+                    Author = blog.Author
+                };
+                blogResources.Add(resource);
+            });
+
+            return Ok(blogResources);
         }
 
+        /// <summary>
+        /// Fetches a specific blog from the database matching the given id param
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Blog with matching id property</returns>
         [HttpGet("{id}")]
         public async Task<IActionResult> GetTaskByIdAsync(int id) {
             var blog = await _context.Blogs.FirstOrDefaultAsync(b => b.BlogId == id);
@@ -71,6 +118,12 @@ namespace BranchServer
             return Ok(blogResource);
         }
 
+        /// <summary>
+        /// Updates a single blog with the information from the resource param
+        /// </summary>
+        /// <param name="id">The id of the blog we are updating</param>
+        /// <param name="blogResource">The contents we want to update with</param>
+        /// <returns>The newly updated blog from the database</returns>
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateBlogAsync(int id, [FromBody] BlogResource blogResource) {
             var updating = await _context.Blogs.FirstOrDefaultAsync(toUpdate => toUpdate.BlogId == id);
@@ -79,6 +132,7 @@ namespace BranchServer
                 return NotFound("Couldn't find a matching blog post.");
             }
 
+            // Update relevant fields, ignoring the id and creation date
             updating.Title = blogResource.Title;
             updating.Body = blogResource.Body;
             updating.Author = blogResource.Author;
