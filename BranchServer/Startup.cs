4aaa7d0b785b3540ffe5af2be01c63e2eadeb74d
Decklan <decklan007@gmail.com>
2018-09-07 14:02:46 -0700

    Added basic authorization to Startup.cs.

--- BranchServer/Startup.cs
+++ BranchServer/Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
 
 namespace BranchServer
 {
@@ -29,6 +32,21 @@ namespace BranchServer
             services.AddDbContext<BranchContext>(options => {
                 options.UseSqlite(Configuration.GetConnectionString("SQLite"));
             });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        RequireExpirationTime = false,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = "http://localhost:62002",
+                        ValidAudience = "http://localhost:4200",
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("developmentKey!@3"))
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +56,7 @@ namespace BranchServer
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
